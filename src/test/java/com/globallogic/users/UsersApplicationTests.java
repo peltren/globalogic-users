0a7e0d775a28ef81eb9da3f2cb32e734e60202ab
peltren <ninibarilatti@gmail.com>
2023-11-12 21:03:25 -0300

    Initial commit

---
+++ src/test/java/com/globallogic/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globallogic.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
